6ae9e2260055c4919833fa3fd2cb4b494453687e
Nursultan Asankadyrov <77961674+coolqwebs@users.noreply.github.com>
2021-02-25 14:02:21 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h4>Aloe Vera King</h4>
+            <h4>Aloe Vera King with some salt</h4>
         </div>
     );
 }
