e1890901ad5bda2d77bf66b4f677c0c7fc3d194c
Nursultan Asankadyrov <77961674+coolqwebs@users.noreply.github.com>
2021-03-02 22:29:13 +0600

    finished components styling block

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,27 +1,42 @@
 import React from "react";
+import Radium from "radium";
+import "./Car.css";
 
 function Car(props) {
-    const divStyle = {
-        display: "block",
-        textAlign: "center",
-        padding: "10px",
-        marginBottom: "10px",
-        border: "1px solid #ccc",
-    };
+    const inputClasses = ["input"];
 
+    if (props.name !== "") {
+        inputClasses.push("green");
+    } else {
+        inputClasses.push("red");
+    }
+    if (props.name.length > 4) {
+        inputClasses.push("bold");
+    }
+
+    const style = {
+        border: "1px solid #ccc",
+        boxShadow: "0 4px 5px rgba(0, 0, 0, 0.14)",
+        ":hover": {
+            border: "1px olid #aaa",
+            boxShadow: "0 4px 15px 0 rgba(0, 0, 0, 0.25)",
+            cursor: "pointer",
+        },
+    };
     return (
-        <div style={divStyle}>
-            <p>Car: {props.name}</p>
+        <div className="Car" style={style}>
+            <h3>Car: {props.name}</h3>
             <p>Year: {props.year}</p>
             <p>Model: {props.model}</p>
             <input
                 type="text"
                 onChange={props.onChangeName}
                 value={props.name}
+                className={inputClasses.join(" ")}
             />
             <button onClick={props.onDelete}>Delete</button>
         </div>
     );
 }
 
-export default Car;
+export default Radium(Car);
